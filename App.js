8e49f1111bb2c442a8a98ed0076bb3b39aad81b7
Earl Mike Sarabia <80509164+esmike03@users.noreply.github.com>
2023-11-08 19:14:10 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -8,87 +8,15 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import UserAvatar from 'react-native-user-avatar';
+import Icon from 'react-native-vector-icons/Entypo';
+import DATA from './src/users';
 
-const DATA = [
-  {
-    id: 1,
-    name: "FELISILDA, ARNOLD JAMES",
-    place: "Singapore, Valencia"
-},
-{
-    id: 2,
-    name: "PAGUICAN, NICOLE JAMES",
-    place: "Canada, Jagna"
-},
-{
-    id: 3,
-    name: "GARCIA, RODEL",
-    place: "Japan, Hanopol"
-},
-{
-    id: 3,
-    name: "TAHIL, ADRIAN JANE",
-    place: "Japan, Hanopol"
-},
-{
-    id: 3,
-    name: "JAGUNOS, JOANA",
-    place: "Japan, Hanopol"
-},
-{
-    id: 3,
-    name: "MIRABITE, MIC ROLAND",
-    place: "Japan, Hanopol"
-},
-{
-    id: 3,
-    name: "CARBONILLA, MARLON",
-    place: "Japan, Hanopol"
-},
-{
-    id: 3,
-    name: "TABANYAG, JOHN",
-    place: "Japan, Hanopol"
-},
-{
-  id: 3,
-  name: "TADLAS, KENNETH",
-  place: "Japan, Hanopol"
-},
-{
-  id: 3,
-  name: "ESCUADRO, NELBREY",
-  place: "Japan, Hanopol"
-},
-{
-  id: 3,
-  name: "BUCIA, WARREN",
-  place: "Japan, Hanopol"
-},
-{
-  id: 3,
-  name: "DONDOYANO, SHERLINE",
-  place: "Japan, Hanopol"
-},
-{
-  id: 3,
-  name: "BONGCARAS, AIREEN MAE",
-  place: "Japan, Hanopol"
-},
-{
-  id: 3,
-  name: "SARABIA, EARL MIKE",
-  place: "Japan, Hanopol"
-},
-];
 
 const Item = ({item, onPress, backgroundColor, textColor}) => (
   <TouchableOpacity onPress={onPress} style={[styles.item, {backgroundColor}]}>
-    <UserAvatar size={50} name="Avishay Bar" />
+    <UserAvatar size={50} name={item.name} />
     <Text style={[styles.title, {color: textColor}]}>{item.name}{'\n'}<Text style={{fontWeight:200}}>{item.place}</Text></Text>
-
-
-    
+    <Icon style={{ marginRight: 10, position: 'absolute', marginLeft: 350}}name="dots-three-horizontal" size={30} color="#000" />;
   </TouchableOpacity>
 );
 
@@ -125,17 +53,21 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 120,
-    alignItems: 'center'
+    marginTop: 20,
+    marginLeft: 15
+    
   },
   item: {
     padding: 10,
-    flexDirection: 'row'
+    flexDirection: 'row',
+    alignItems: 'center'
 
   },
   title: {
     fontSize: 18,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    marginLeft: 20
+   
   },
   place: {
 
---
+++ src/users.js
@@ -0,0 +1,74 @@
+const DATA = [
+    {
+      id: 0,
+      name: "FELISILDA, ARNOLD JAMES",
+      place: "Singapore, Valencia"
+  },
+  {
+      id: 1,
+      name: "PAGUICAN, NICOLE JAMES",
+      place: "Canada, Jagna"
+  },
+  {
+      id: 2,
+      name: "GARCIA, RODEL",
+      place: "Japan, Hanopol"
+  },
+  {
+      id: 3,
+      name: "TAHIL, ADRIAN JANE",
+      place: "Hungary, Hanopol"
+  },
+  {
+      id: 4,
+      name: "JAGUNOS, JOANA",
+      place: "Japan, Hanopol"
+  },
+  {
+      id: 5,
+      name: "MIRABITE, MIC ROLAND",
+      place: "Iceland, Tagbilaran"
+  },
+  {
+      id: 6,
+      name: "CARBONILLA, MARLON",
+      place: "Dominican Republic, Maribojoc"
+  },
+  {
+      id: 7,
+      name: "TABANYAG, JOHN",
+      place: "Timor-Leste, Cortes"
+  },
+  {
+    id: 8,
+    name: "TADLAS, KENNETH",
+    place: "Nicaragua, Dao"
+  },
+  {
+    id: 9,
+    name: "ESCUADRO, NELBREY",
+    place: "Italy, Cortes"
+  },
+  {
+    id: 10,
+    name: "BUCIA, WARREN",
+    place: "Denmark, Cortes"
+  },
+  {
+    id: 11,
+    name: "DONDOYANO, SHERLINE",
+    place: "Luxembourg, Balilihan"
+  },
+  {
+    id: 12,
+    name: "BONGCARAS, AIREEN MAE",
+    place: "Finland, Dauis"
+  },
+  {
+    id: 13,
+    name: "SARABIA, EARL MIKE",
+    place: "United Kingdom, Dampas"
+  },
+  ];
+
+  export default DATA;
