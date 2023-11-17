33aec88c46cd8c2764ad16b8469345b1844571e3
Earl Mike Sarabia <80509164+esmike03@users.noreply.github.com>
2023-11-17 21:42:51 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -5,32 +5,42 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  Modal,
   TouchableOpacity,
+  Alert,
+  View
 } from 'react-native';
 import UserAvatar from 'react-native-user-avatar';
 import Icon from 'react-native-vector-icons/Entypo';
 import DATA from './src/users';
-
-
-const Item = ({item, onPress, backgroundColor, textColor}) => (
-  <TouchableOpacity onPress={onPress} style={[styles.item, {backgroundColor}]}>
-    <UserAvatar size={50} name={item.name} />
-    <Text style={[styles.title, {color: textColor}]}>{item.name}{'\n'}<Text style={{fontWeight:200}}>{item.place}</Text></Text>
-    <Icon style={{ marginRight: 10, position: 'absolute', marginLeft: 350}}name="dots-three-horizontal" size={30} color="#000" />;
-  </TouchableOpacity>
-);
-
-const App = () => {
+export default function App() {
   const [selectedId, setSelectedId] = useState();
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null);
+
+  const show = (item) =>{
+    setModalVisible(true);
+    setSelectedUser(item);
+  };
+
+  const Item = ({item, onPress, backgroundColor, textColor}) => (
+  
+    <TouchableOpacity onPress={onPress} style={[styles.item, {backgroundColor}]}> 
+      <UserAvatar size={50} name={item.name} />
+      <Text style={[styles.title, {color: textColor}]}>{item.name}{'\n'}<Text style={{fontWeight:200}}>{item.place}</Text></Text>
+      <Icon style={{ marginRight: 10, position: 'absolute', marginLeft: 350}} name="dots-three-horizontal" size={30} color="#000" />;
+    </TouchableOpacity>
+  
+  );
 
   const renderItem = ({item}) => {
-    const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#fffff';
+    const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#ffff';
     const color = item.id === selectedId ? 'white' : 'black';
 
     return (
       <Item
         item={item}
-        onPress={() => setSelectedId(item.id)}
+        onPress={() => show(item) }
         backgroundColor={backgroundColor}
         textColor={color}
       />
@@ -38,14 +48,44 @@ const App = () => {
   };
 
   return (
+    
     <SafeAreaView style={styles.container}>
       
+        <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false);
+        }}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+                {selectedUser && (
+                  <Text style={{fontSize: 18, fontWeight: '500'}}>HELLO! {selectedUser.name}.
+                  </Text>
+                )}
+                {selectedUser && (
+                  <Text style={{fontWeight: '100'}}>{selectedUser.place}</Text>
+                )}
+
+                <TouchableOpacity onPress={()=> setModalVisible(false)}>
+                  <Text style={{marginTop: 25, fontSize: 15, color: '#776B5D'}}>Hide</Text>
+                </TouchableOpacity>
+                  
+                   
+               
+            </View>
+          </View>
+        </Modal>
+
       <FlatList
         data={DATA}
         renderItem={renderItem}
         keyExtractor={item => item.id}
         extraData={selectedId}
       />
+
+    
     </SafeAreaView>
   );
 };
@@ -54,7 +94,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 20,
-    marginLeft: 15
+    marginLeft: 0
     
   },
   item: {
@@ -63,15 +103,31 @@ const styles = StyleSheet.create({
     alignItems: 'center'
 
   },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.50,
+    shadowRadius: 4,
+    elevation: 5,
+  },
   title: {
     fontSize: 18,
     fontWeight: 'bold',
     marginLeft: 20
    
   },
-  place: {
-
-  }
 });
-
-export default App;
